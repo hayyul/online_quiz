ccf151d13404134390194882ea32f3f5dc0858e0
Alican Ali <44370554+AliAlican@users.noreply.github.com>
2022-04-05 16:17:18 +0200

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,3 +1,3 @@
 <?php
-	$con= new mysqli('localhost','root','','online-quiz-system')or die("Could not connect to mysql".mysqli_error($con));
+	$con= new mysqli('localhost','root','','onlinequizsystem')or die("Could not connect to mysql".mysqli_error($con));
 ?>
